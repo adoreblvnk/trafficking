61900ae25feb8eb7905a1c95073136dc22765efa
adoreblvnk <83258252+adoreblvnk@users.noreply.github.com>
2026-01-17 17:11:11 +0800

    docs: add note for modified boilerplate file

--- lwjgl3/src/main/java/com/sit/trafficking/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/sit/trafficking/lwjgl3/Lwjgl3Launcher.java
@@ -28,6 +28,7 @@ public class Lwjgl3Launcher {
         //// useful for testing performance, but can also be very stressful to some hardware.
         //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
 
+        // NOTE: bigger window size!
         configuration.setWindowedMode(1280, 720);
         //// You can change these files; they are in lwjgl3/src/main/resources/ .
         //// They can also be loaded from the root of assets/ .
